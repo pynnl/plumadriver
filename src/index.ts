3edef37e84cd06b80a6f640f6512a0e3545ba447
pynnl <pynnl@outlook.com>
2020-09-15 13:38:01 -0400

    fix: issue 114

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { app } from './app';
 
 const args = argv(process.argv.slice(2));
 const HTTP_PORT = process.env.PORT || args.port || 3000;
-
+//FIXED
 app.listen(HTTP_PORT, () => {
   console.log(`plumadriver is listening on port ${HTTP_PORT}`);
 });
